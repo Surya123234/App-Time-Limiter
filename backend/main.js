88cd3322ccd233e7ab38e9dc00b6870b97e743fe
CodeYimin <56568926+CodeYimin@users.noreply.github.com>
2021-08-14 15:21:02 -0400

    setup cors

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+const corsOptions = cors({
+  origin: '*',
+})
+
+app.use(corsOptions)
 
 app.get('/', (req, res) => {
   res.send('Hello world');
