dcf6c44b4173b5d779771cdc1a7cc6552f92230a
CodeYimin <56568926+CodeYimin@users.noreply.github.com>
2021-08-14 14:13:57 -0400

    Express setup

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+})
+
+app.listen(port, () => {
+  console.log(`Listening to http://localhost:${port}`);
+})
