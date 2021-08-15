b449c9a63f3c28b93f9ad26be2075a9a7679e56c
Surya <surya.sendhilraj@gmail.com>
2021-08-15 00:05:58 -0400

    added a few input fields, sent a message to background.js

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,17 +1,1 @@
 console.log("hello world")
-
-async function testApiCall() {
-  const response = await fetch('http://localhost:3000');
-  return await response.text();
-}
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  switch (message.action) {
-    default:
-      break
-    case 'testFetch':
-      testApiCall().then(sendResponse);
-      return true;
-      break;
-  }
-})
--- chrome-extension/popup/popup.js
+++ chrome-extension/popup/popup.js
@@ -1,13 +1,24 @@
 document.addEventListener('DOMContentLoaded', onLoad)
 
 function onLoad() {
-  const testButton = document.getElementById('test');
-  testButton.addEventListener('click', () => {
-    chrome.runtime.sendMessage({action: "testFetch"}, (response) => {
-      responseText = document.getElementById('responseText');
-      responseText.innerHTML = response;
-    })
-  })
+  document.getElementById("form").addEventListener("submit", handleSubmit, false);
 
 }
+
+
+
+function handleSubmit() {
+  chrome.runtime.sendMessage({
+    "action": "limit_website_usage",
+    "website": document.querySelector("#website").value,
+    "hours": document.querySelector("#duration-hours").value,
+    "minutes": document.querySelector("#duration-minutes").value
+  }, (res) => {
+    console.log(res);
+  })
+}
+
+
+
+
 
