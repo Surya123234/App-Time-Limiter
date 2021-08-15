9ee1d37a0b9bc9b2d0d2060c66e804c0b26fc29c
CodeYimin <56568926+CodeYimin@users.noreply.github.com>
2021-08-15 00:33:17 -0400

    remove electron

--- backend/main.js
+++ backend/main.js
@@ -1,44 +1,43 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-require('./server');
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const cors = require('cors');
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+const app = express();
+app.use(cors());
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+//initialize a simple http server
+const server = http.createServer(app);
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+function blockDiscord(ws) {
+  ws.send('block');
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+function unblockDiscord(ws) {
+  ws.send('unblock');
+}
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+wss.on('connection', (ws) => {
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+    ws.on('message', (message) => {
+      if (JSON.parse(message).origin === 'discord') {
+        // Delay to demonstarte its working
+        setTimeout(() => {
+          unblockDiscord(ws)
+        }, 5000)
+        setTimeout(() => {
+          blockDiscord(ws)
+        }, 10000)
+      }
+    });
+
+});
+
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
